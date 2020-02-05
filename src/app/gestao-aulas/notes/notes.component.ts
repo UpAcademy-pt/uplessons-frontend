d74626f1ffa0987b83cecc3bcb792b593aef10fe
tifipe <tiago_preto_20@hotmail.com>
2020-02-05 18:22:07 +0000

    script pdf notas

--- src/app/gestao-aulas/notes/notes.component.ts
+++ src/app/gestao-aulas/notes/notes.component.ts
@@ -9,7 +9,10 @@ import { ReplaySubject } from 'rxjs';
 import { DataService } from '../shared/services/data.service';
 import { NotesService } from '../shared/services/notes.service';
 import { faEdit, faTrashAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import html2canvas from 'html2canvas';
+import { ScriptPdfService } from '../shared/services/scriptPdf/script-pdf.service';
 
+declare let pdfMake: any;
 
 
 @Component({
@@ -18,6 +21,8 @@ import { faEdit, faTrashAlt, faUserPlus } from '@fortawesome/free-solid-svg-icon
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
+  title1 = 'PdfExample';
+
   modalRef: BsModalRef;
   private notes: Note[];
   private note: Note = new Note();
@@ -41,8 +46,13 @@ export class NotesComponent implements OnInit {
     private accountApi: ServiceGeneralService,
     private notesAPI: NotesService,
     private modalService: BsModalService,
-    private dataService: DataService
+    private dataService: DataService,
+    private scriptService: ScriptPdfService
   ) {
+    this.scriptService.load('pdfMake');
+    setTimeout(() => {
+      this.scriptService.load( 'vfsFonts');
+    }, 500);
     this.dataService.getNotesByAccountId(this.accountApi.getCurrentAccountId());
     this.notes = dataService.notes;
     this.notes$ = dataService.notes$;
@@ -109,6 +119,69 @@ export class NotesComponent implements OnInit {
     
     // this.noteToUpdate = new Note();
   }
+
+  generatePdf() {
+    // const opt = { letterRendering: 1, allowTaint: true, imageTimeout: 10000, onrendered: (canvas) => { } };
+    // html2canvas(document.querySelector('#capture'), opt).then(canvas => {
+    //   const img = canvas.toDataURL();
+
+      /* const documentDefinition = {
+        content: {
+          image: img,
+          width: 520
+        }
+      }; */
+
+      
+      // var documentDefinition = {
+      //   content: [
+      //     {
+      //       layout: 'lightHorizontalLines', // optional
+      //       table: {
+      //         // headers are automatically repeated if the table spans over multiple pages
+      //         // you can declare how many rows should be treated as headers
+      //         headerRows: 1,
+      //         widths: [ '*', 'auto', 100, '*' ],
+      
+      //         body: [
+      //           [ 'First', 'Second', 'Third', 'The last one' ],
+      //           [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+      //           [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+      //         ]
+      //       }
+      //     }
+      //   ]
+      // };
+     var documentDefinition = {
+  content: [
+    'Bulleted list example:',
+    {
+      // to treat a paragraph as a bulleted list, set an array of items under the ul key
+      ul: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        { text: 'Item 4', bold: true },
+      ]
+    },
+
+    'Numbered list example:',
+    {
+      // for numbered lists set the ol key
+      ol: [
+        'Item 1',
+        'Item 2',
+        'Item 3'
+      ]
+    }
+  ]
+};
+
+      pdfMake.createPdf(documentDefinition).open();
+   // });
+  }
+
+
 }
 
 
