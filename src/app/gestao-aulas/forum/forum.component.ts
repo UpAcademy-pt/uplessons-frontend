b2e25578c79ccacf42dd778abd67b6f8f9eb0b10
miguelramirezcordeiro <miguelramirezcordeiro@gmail.com>
2020-02-06 12:24:21 +0000

    add forum display

--- src/app/gestao-aulas/forum/forum.component.ts
+++ src/app/gestao-aulas/forum/forum.component.ts
@@ -103,4 +103,10 @@ export class ForumComponent implements OnInit {
 
   }
 
+  public answerDisplay = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']
+  public userAnswer = ['João Cortes', 'Cristiano Vale', 'Tiago Preto', 'Sofia Lucas', 'Ana Cardoso']
+
+  public getRandomUser() {
+    return this.userAnswer[Math.floor(Math.random() * this.userAnswer.length)];
+  }
 }
